4b39fd34bf49fc59bfb6d03ab80d9aa948646153
AdamRB208 <adamrborup@gmail.com>
2025-07-25 11:59:49 -0600

    init

---
+++ client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:7045' : ''
+export const useSockets = false
+export const domain = 'dev-d3qj26bqdtixpiu0.us.auth0.com'
+export const clientId = 'rX26uYnOTmtdPnaKkWDK8b8KiA1HbTLi'
+export const audience = 'https://auth.optamasticprimefoundation.com'
