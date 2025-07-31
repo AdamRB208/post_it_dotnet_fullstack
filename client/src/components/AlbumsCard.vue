716f0f403db0a13f6346a0429dbb540760cbba85
AdamRB208 <adamrborup@gmail.com>
2025-07-31 13:15:57 -0600

    created albums page, added to router and set up router link.

--- client/src/components/AlbumsCard.vue
+++ client/src/components/AlbumsCard.vue
@@ -9,19 +9,20 @@ defineProps({
 
 
 <template>
-  <!-- <RouterLink > -->
-  <div class="rounded shadow d-flex align-items-end text-light album-card mb-2 mt-2" :style="{ backgroundImage: `url(${album.coverImg})` }">
-    <div class="d-flex justify-content-between align-items-center rounded bg-dark p-2 flex-grow-1">
-      <div>
-        <p>{{ album.title }}</p>
-      </div>
-      <div>
-        <img :src="album.creator.picture" :alt="'A picture of ' + album.creator.name" class="rounded-5 me-2">
-        <span>{{ album.creator.name }}</span>
+  <RouterLink :to="{ name: 'Albums Page', params: { albumId: album.id } }" :title="`Go to the ${album.title}'s page!`">
+    <div class="rounded shadow d-flex align-items-end text-light album-card mb-2 mt-2"
+      :style="{ backgroundImage: `url(${album.coverImg})` }">
+      <div class="d-flex justify-content-between align-items-center rounded bg-dark p-2 flex-grow-1">
+        <div>
+          <p>{{ album.title }}</p>
+        </div>
+        <div>
+          <img :src="album.creator.picture" :alt="'A picture of ' + album.creator.name" class="rounded-5 me-2">
+          <span>{{ album.creator.name }}</span>
+        </div>
       </div>
     </div>
-  </div>
-  <!-- </RouterLink> -->
+  </RouterLink>
 </template>
 
 
---
+++ client/src/pages/AlbumsPage.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <div>Albums page loads!</div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
