793d7ed58c0169313a14268a376a08bf7bff700f
AdamRB208 <adamrborup@gmail.com>
2025-08-05 11:28:59 -0600

    updated picture styling and added create button.

--- client/src/components/PictureCard.vue
+++ client/src/components/PictureCard.vue
@@ -11,12 +11,17 @@ const props = defineProps({
 
 <template>
   <div class="position-relative">
-    <img :src="picture.imgUrl" :alt="'Picture submitted by ' + picture.creator.name" class="w-100">
-    <button class="btn btn-danger rounded-circle position-absolute" title="Delete your picture" type="button"><span class="mdi alpha-x-circle-outline"></span></button>
-    <div class="position-absolute">
-      <img :src="picture.creator.name" :alt="'A picture of ' + picture.creator.name" class="round-picture me-2" height="30">
+    <img :src="picture.imgUrl" :alt="'Picture submitted by ' + picture.creator.name" class="w-100 rounded-4">
+    <button class="btn btn-danger btn-sm rounded-circle position-absolute mt-1 me-1" title="Delete your picture"
+      type="button"><span class="mdi mdi-close-circle-outline text-light"></span></button>
+    <div class="position-absolute d-flex m-1">
+      <img :src="picture.creator.picture" :alt="'A picture of ' + picture.creator.name" class="me-2 rounded-circle"
+        height="30">
       <div>{{ picture.creator.name }}</div>
     </div>
+  </div>
+  <div class="d-flex justify-content-end">
+    <button class="btn btn-vue rounded-pill mt-3 text-light " type="button">Add Picture</button>
   </div>
 </template>
 
