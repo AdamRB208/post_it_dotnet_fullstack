0d902bc74f980da93a23782a8d6b2d05601681f5
AdamRB208 <adamrborup@gmail.com>
2025-08-06 15:57:54 -0600

    Updated Watcher model.

--- client/src/models/Watcher.js
+++ client/src/models/Watcher.js
@@ -1,9 +1,26 @@
-export class Watcher {
+import { Profile } from "./Account.js"
+import { Album } from "./Album.js"
+
+class Watcher {
   constructor(data) {
     this.id = data.id
     this.createdAt = new Date(data.createdAt)
     this.updatedAt = new Date(data.updatedAt)
     this.accountId = data.accountId
     this.albumId = data.albumId
+  }
+}
+
+export class WatcherProfile extends Watcher {
+  constructor(data) {
+    super(data)
+    this.profile = new Profile(data.profile)
+  }
+}
+
+export class WatcherAlbum extends Watcher {
+  constructor(data) {
+    super(data)
+    this.album = new Album(data.album)
   }
 }
